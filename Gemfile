1fe40eb7aa48367c524741003b5d77c4b9ae973e
Matt Briggs <matt@mattbriggs.net>
2013-08-19 10:23:57 -0400

    initial

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sqlite3'
+gem 'thin'
+gem 'quiet_assets'
+gem 'better_errors'
+gem 'active_model_serializers'
+
+group :development,:test,:assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'anjlab-bootstrap-rails', require: 'bootstrap-rails', github: 'anjlab/bootstrap-rails', branch: '3.0.0'
+  gem 'font-awesome-rails'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarionetteTableExample::Application.config.secret_key_base = '5d2be95bbb5555d02276539cae0ab2250768ca0173824e04756348d4425c19229ed1d39b68b88be2ff755055f6715f0cf2557e653814c6541f56b7bfd83cdc04'
